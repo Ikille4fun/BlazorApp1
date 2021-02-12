875e48bad1507614f1094aaf195eeecb40749351
alexander <alexander.hoffmann@maxmanus.com>
2021-02-12 16:06:03 +0100

    Did not get these: AudioSave.cs, AudioRecorder.js and CreateSaveFile.js in the previous commit

---
+++ BlazorApp1/Client/Components/AudioSave.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+
+namespace BlazorApp1.Client
+{
+    public class AudioSave : Controller
+    {
+        IHostingEnvironment _hostingEnvironment;
+
+        public AudioSave(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [Route("api/[controller]/Save")]
+        [HttpPost]
+        public async Task<IActionResult> Save(IFormFile file)
+        {
+            if (file.ContentType != "audio/wav")
+            {
+                return BadRequest("Wrong file type");
+            }
+            var uploads = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");//uploads where you want to save data inside wwwroot
+
+            //var filePath = Path.Combine(uploads, Path.GetRandomFileName() + ".wav");
+            var filePath = Path.Combine(uploads, file.FileName + ".wav");
+            await using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+            return Ok("File uploaded successfully");
+        }
+    }
+}
---
+++ BlazorApp1/Client/wwwroot/Js/CreateSaveFile.js
@@ -0,0 +1,27 @@
+﻿function createSaveFile(blob) {
+    //var au = document.createElement("audio");
+    let au = document.querySelector("audio");
+
+    wav = window.URL.createObjectURL(blob);
+    au.src = window.URL.createObjectURL(blob);
+    let filename = new Date().toISOString().replaceAll(":", "");
+    let fd = new FormData();
+    fd.append("file", blob, filename);
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", transferComplete);
+    xhr.addEventListener("error", transferFailed);
+    xhr.addEventListener("abort", transferFailed);
+    xhr.open("POST", "api/Components/AudioSave/Save/", true);
+    xhr.send(fd); //it fails her post 500 error
+}
+
+function transferComplete(evt) {
+    console.log("The transfer is complete.");
+}
+
+function transferFailed(evt) {
+    console.log("An error occurred while transferring the file.");
+
+    console.log(evt.responseText);
+    console.log(evt.status);
+}
